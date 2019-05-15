9fa373b8a0242f4492185b1cc0d93ecd80b5064c
Javierpablo <39129323+Pablojvr@users.noreply.github.com>
2019-05-14 21:37:41 -0600

    Hola, soy el CEO aportando

--- private/consultar.php
+++ private/consultar.php
@@ -1,1 +1,29 @@
-Consultar Papu
+<?php
+    define("Raiz",$_SERVER['DOCUMENT_ROOT']); //Se obtiene la raiz del sitio para facilitar los include
+    include_once(Raiz."/ProyectoPHP/lib/conexion.php");
+    $conn = new conexion;
+    $id = $_COOKIE['Id'];
+   $rs = $conn->ExecuteQuery("SELECT c.Saldo, c.NCuenta FROM cuenta c WHERE IdUsuario = $id");
+
+?>
+
+<table class="table table-striped table-bordered" id="HistorialTable">
+    <thead>
+        <tr>
+            <th>Nombre de cuenta</th>
+            <th>Monto total</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach($rs as $x):?>
+        <tr>
+            <td><?=$x["NCuenta"]?></td>
+            <td><?=$x["Saldo"]?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+
+<script>
+        $('#HistorialTable');
+</script>
